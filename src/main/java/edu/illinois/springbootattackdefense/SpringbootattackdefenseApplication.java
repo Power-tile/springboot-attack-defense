fd5cf19500064ef72ff08ce4243ae9c6edf3b8d1
Power-tile <hejizheng2004@163.com>
2021-12-08 16:21:58 -0600

    Initial Commit

---
+++ src/main/java/edu/illinois/springbootattackdefense/SpringbootattackdefenseApplication.java
@@ -0,0 +1,13 @@
+package edu.illinois.springbootattackdefense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootattackdefenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootattackdefenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/illinois/springbootattackdefense/SpringbootattackdefenseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.illinois.springbootattackdefense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootattackdefenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
